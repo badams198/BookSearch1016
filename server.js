0ee9bb6d5234ecf63b6eb54c72459d594b4f4810
Badams <badams10@gmail.com>
2019-10-09 08:18:03 -0500

    initial commit

--- server.js
+++ server.js
@@ -1,31 +1,23 @@
+require('dotenv').config()
 const express = require("express");
+const mongoose = require("mongoose");
 const path = require("path");
-const db = require("./models")
 const PORT = process.env.PORT || 3001;
 const app = express();
-require("dotenv");
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/gbooks";
+const routes = require("./routes/index.js");
 
-// Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-// Define API routes here
-require('./routes/api/routes')(app);
+app.use("/", routes);
 
-// Send every other request to the React app
-// Define any API routes before this runs
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
 
-// Syncing our sequelize models and then starting our Express app
-db.sequelize.sync({ force: false }).then(function() {
-  app.listen(PORT, function() {
-    console.log(`API server running @ https://localhost:${PORT}`);
-  });
+app.listen(PORT, () => {
+  console.log(`🌎 ==> API server now on port ${PORT}!`);
 });
